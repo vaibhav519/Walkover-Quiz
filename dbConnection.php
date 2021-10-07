bd7245ea0c21008e4f0f07b96f348730e26f6bb7
Sneh Joshi <snehj814@gmail.com>
2021-10-07 11:04:28 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,7 @@
-<?php
-	$con= new mysqli('sql301.epizy.com', 'epiz_29814815', 'DWOvkF1aocq4tVv', 'epiz_29814815_quiz')or die("Could not connect to mysql".mysqli_error($con));
+<?php 
+      /*Development Connection
+ 	$con= new mysqli('sql301.epizy.com', 'epiz_29814815', 'DWOvkF1aocq4tVv', 'epiz_29814815_quiz')or die("Could not connect to mysql".mysqli_error($con));*/
+
+      /*Remote Connection*/
+        $con= new mysqli('remotemysql.com', 'ncIPcB4rtI', 'ufMijmFRfR', 'ncIPcB4rtI')or die("Could not connect to mysql".mysqli_error($con));
 ?>
