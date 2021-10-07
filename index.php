9d8e0784e75ab605f0f499d5678046b024bcfbe2
Sneh Joshi <snehj814@gmail.com>
2021-10-07 10:28:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -295,7 +295,7 @@ if (isset($_SESSION['username']) && (!isset($_SESSION['key']))) {
   </div>
   <div class="row footer">
     <div class="col-md-2 box">
-      <a href="#" data-toggle="modal" data-target="#login" style="color:lightyellow;font-weight:bolder;">Admin Login</a><br>
+      <a href="#" data-toggle="modal" data-target="#login" style="color:lightyellow;font-weight:bolder;">Admin Login</a><br><br>
     </div>
     <div class="col-md-2 box">
       <a href="feedback.php" style="color:lightyellow;font-weight:bolder;" onmouseover="this.style('color:yellow')" target="new">Feedback</a>
